7fda245c4e3f6ee36429c54866d0b87359345fb8
EdgardoHidalgo <jugador.eo@gmail.com>
2024-11-21 19:02:17 +0100

    si

---
+++ ol.java
@@ -0,0 +1,23 @@
+public class ol {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] sumArray = {4,36,4,23,867};
+        int[] subArray = {785675, 56, 5675, 56544, 234};
+        int[] prodArray = {6, 8, 20, 23, 12};
+        int sum = 0;
+
+        // User 1 fer la suma dels elements de sumArray
+      for (int i = 0; i < sumArray.length; i++) {
+          sum+=sumArray[i];
+
+      }
+        System.out.println(sum);
+
+
+        // User 2 fer la resta dels elements de subArray
+
+
+
+        // User 3 fer la multiplicació dels elements de prodArray
+    }
+}
