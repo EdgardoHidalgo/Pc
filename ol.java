4f3e0729b4c242b987389742839b44b19d1d9c95
EdgardoHidalgo <jugador.eo@gmail.com>
2024-11-21 19:06:24 +0100

    RESTA Y MULTIPLICACION

--- ol.java
+++ ol.java
@@ -5,19 +5,26 @@ public class ol {
         int[] subArray = {785675, 56, 5675, 56544, 234};
         int[] prodArray = {6, 8, 20, 23, 12};
         int sum = 0;
+        int resta = 0;
+        int mult = 1;
 
         // User 1 fer la suma dels elements de sumArray
       for (int i = 0; i < sumArray.length; i++) {
           sum+=sumArray[i];
 
       }
-        System.out.println(sum);
+        System.out.println("La suma del array es :" +sum);
 
 
         // User 2 fer la resta dels elements de subArray
-
-
-
+        for (int i = 0; i < subArray.length; i++) {
+            resta-=subArray[i];
+        }
+        System.out.println("La resta del array es :" + resta);
         // User 3 fer la multiplicació dels elements de prodArray
+        for (int i = 0; i < subArray.length; i++) {
+          mult*=prodArray[i];
+        }
+        System.out.println("La mult del array es :" +mult);
     }
 }
